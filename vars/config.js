45ce9a70692e8c825349e289b94d2bb10aadc239
naxor <naxordeve@gmail.com>
2025-03-14 15:22:45 +0200

    Create config.js

---
+++ vars/config.js
@@ -0,0 +1,5 @@
+export const devApiKey = "ypkqXUGgzysc_yLPTBaEZ_G3G-nvjEsh";  
+export const username = "Naxordeve";  
+export const user_password = "Naxordeve@127";  
+export const authPath = "../session/creds.json";  
+export const getPrefa = () => "Aqua~";  
